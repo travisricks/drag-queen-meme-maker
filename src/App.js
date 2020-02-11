0d4b28a49183e6d7e00af8ecf094c39d41920956
Travis Ricks <travis@travisricks.com>
2020-02-11 12:02:47 +0900

    add faker content for mocking

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React, {useEffect} from "react";
+import logo from "./logo.svg";
+import "./App.scss";
 
-import {getQuotes} from "./utils/index";
+import { getQuotes } from "./utils/index";
+import { generateFakes } from "./utils/faker";
+
 console.log(getQuotes());
+console.log(generateFakes());
 function App() {
+  const fakeData = generateFakes();
+  console.log('fakeData', fakeData)
+  console.log('fakeData[0].quoteImage', fakeData[0].quoteImage)
 
   return (
     <div className="App">
@@ -13,6 +19,9 @@ function App() {
         <p>
           stuff Edit <code>src/App.js</code> and save to reload.
         </p>
+        <img src={fakeData[0].quoteImage} alt="quote"/>
+        <img src={fakeData[3].quoteImage} alt="quote"/>
+        <img src={fakeData[8].quoteImage} alt="quote"/>
         <a
           className="App-link"
           href="https://reactjs.org"
